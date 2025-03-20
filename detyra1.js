3de9fc29340aeb96c5eaa9ddc708cb196b7445d5
ArbIsmajli <ismajliarb@gmail.com>
2025-03-20 19:43:39 +0100

    ktu e kom kry pak detyren po ala kom

---
+++ detyra1.js
@@ -0,0 +1,17 @@
+// const button = document.getElementById("buttonIg");
+// const login = document.querySelector("#input");
+// const input = document.querySelector("#main");
+
+// function test(){
+//     console.log("test");
+
+//     login.style.display = 'none';
+//     input.style.display = 'block';
+    
+// }
+
+document.getElementById('input').addEventListener('submit', function(event) {
+    event.preventDefault();
+    var username = document.getElementById('username').value;
+
+})
