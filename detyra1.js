9be7b576a2558010df4945ca2d16481d8eef8f72
ArbIsmajli <ismajliarb@gmail.com>
2025-04-01 19:43:52 +0200

    ktu duhna me vazhdu

--- detyra1.js
+++ detyra1.js
@@ -1,17 +1,27 @@
-// const button = document.getElementById("buttonIg");
-// const login = document.querySelector("#input");
-// const input = document.querySelector("#main");
+const button = document.getElementById("buttonIg");
+const login = document.querySelector(".login");
+const main = document.querySelector(".main");
+const loginMessage = document.getElementById('loginmessage')
 
-// function test(){
-//     console.log("test");
+function test(){
+    console.log("test");
 
-//     login.style.display = 'none';
-//     input.style.display = 'block';
+    login.style.display = 'none';
+    input.style.display = 'block';
     
-// }
+}
 
-document.getElementById('input').addEventListener('submit', function(event) {
+button.addEventListener('click', function() {
     event.preventDefault();
     var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    console.log(username, password);
 
-})
+    if ((username === 'arb' && password === '1234') || (username === 'Arb' && password === '4321')) {
+        loginMessage.textContent = '';
+        login.style.display = 'none';
+        main.style.display = 'block';
+    } else {
+        loginMessage.textContent = 'perdoruesi ose fjalkalimi jane te pasakta';
+    }
+});
